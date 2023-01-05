7d630d2d11c9def8dc1ef381aa1e203566325304
FlavioCTC <fhuaccho@crackthecode.la>
2023-01-05 18:54:13 -0500

    Primer commit

---
+++ js/modulos.js
@@ -0,0 +1,12 @@
+function saludo(user) {
+    return(`Hola, ${user}!`);
+}
+
+function sumar(a,b){
+    return a+b;
+}
+
+const valorPi= Math.PI;
+
+
+export {saludo, sumar, valorPi};
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import {saludo, sumar, valorPi} from './modulos.js';
+
+console.log(saludo("Crack The Code"));
+console.log(sumar(4,2));
+console.log(valorPi);
